e3194f35deb41dcf9012ecab486f54129982da04
ijosepham <44876783+ijosepham@users.noreply.github.com>
2019-05-15 14:14:25 -0700

    updated to do list. i needa study for 229 now. i'll keep working after i get home

--- 277 Final Exam/src/Main.java
+++ 277 Final Exam/src/Main.java
@@ -5,12 +5,8 @@ import frames.*;
 /**
  * to-do: 
  * check in and out
- * creating rooms all at once? or when called
- * 		make thme all at once
- * waitlists belongs to the room itself
- * do waitlisted people need a reservation, or just guest info?
- * 		reservation needs time
- * 		resrevation is cancelled if the whole entire tiem lot is not open
+ * resrevation is cancelled if the whole entire tiem lot is not open
+ * prices.
  */
 
 public class Main {
